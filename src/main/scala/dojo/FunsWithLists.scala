478a488a12c72cb2c01252a055ddc0581d95ca79
Peter Crotty <peter@crotty.co.uk>
2012-03-27 13:10:10 +0100

    Next few tests (probably messier implementation than necessary).

--- src/main/scala/dojo/FunsWithLists.scala
+++ src/main/scala/dojo/FunsWithLists.scala
@@ -15,19 +15,34 @@ object FunsWithLists {
 //  }
 
   def averageRatingsOf(l:String,  ls:List[Game]) = {
-    //need to filter this
-    totalRatingsOf(ls)  / ls.length
+    var runningTotal = 0
+    var count = 0
+    for (game <- ls if game.label.equals(l)) {
+      runningTotal += game.rating;
+      count += 1;
+    }
+    runningTotal / count
   }
 
   def totalRatingsOf(ls: List[Game]):Int = {
-    ls.map(item =>item.rating).sum
+    ls.map(item => item.rating).sum
   }
 
-  def totalRatingsOfLabel(label: String, list: List[Game]):Int = 0
+  def totalRatingsOfLabel(label: String, list: List[Game]):Int = {
+    var runningTotal = 0
+    for (game <- list if game.label.equals(label)) {
+      runningTotal += game.rating;
+    }
+    runningTotal
+}
 
-  def increaseRatingBy(inc: Int, ls: List[Game]) = Nil
+  def increaseRatingBy(inc: Int, ls: List[Game]) = {
+    ls.map(item => Game(item.label, item.rating + inc))
+  }
 
-  def decreaseRatingBy(i: Int, s: String, list: List[Game]) = Nil
+  def decreaseRatingBy(i: Int, s: String, list: List[Game]) = {
+    list.map(item => if (item.label.equals(s)) Game(item.label, item.rating - i) else item)
+  }
 
   def createFunctionToFindGamesByLabel(label: String):(List[Game]) => List[Game] = identity(_)
 
